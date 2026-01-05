2ab9592075f06ad5161ec1c13c7dbd3080e3dca3
spring1199 <moojig_0908@yahoo.com>
2026-01-05 14:09:18 +0800

    Fix: Use process.env.PORT to allow Render to assign port

--- server/index.js
+++ server/index.js
@@ -79,7 +79,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
